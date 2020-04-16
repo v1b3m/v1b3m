9c52d1d98de5e42a296a79761cc017fb4d236c8e
v1b3m <vibenjamin6@gmail.com>
2020-04-16 08:27:01 +0300

    Updates

---
+++ precache-manifest.edb8c64a285b4a536e56bbefe124051e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bb840de32597654f8f28debd827d222",
+    "url": "/v1b3m/index.html"
+  },
+  {
+    "revision": "8b7837f4d8878e1ac0a9",
+    "url": "/v1b3m/static/css/main.75cfdd92.chunk.css"
+  },
+  {
+    "revision": "f52bb864d803bdc4cc2d",
+    "url": "/v1b3m/static/js/2.2a42f51a.chunk.js"
+  },
+  {
+    "revision": "ce7ab8e66e79afadef656da3c5775fa6",
+    "url": "/v1b3m/static/js/2.2a42f51a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b7837f4d8878e1ac0a9",
+    "url": "/v1b3m/static/js/main.284ba802.chunk.js"
+  },
+  {
+    "revision": "d783ebbb645de4b18671",
+    "url": "/v1b3m/static/js/runtime-main.1474816e.js"
+  }
+]);
